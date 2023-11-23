3f0167e6bdf9ece302a961e8f45ea3a4332edd2f
roslynpius <roslyn.pius@gmail.com>
2023-11-23 16:00:17 +0530

    Implemented USE CASE 2: Added Daily Employee Wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -9,12 +9,28 @@ public class Employee_wage {
         Random random = new Random();
         final int FULL_TIME=1;
         int isPresent = random.nextInt(2); // 0 for absent, 1 for full time present
+        int workingHours=0;
 
         if (isPresent == FULL_TIME){
             System.out.println("Employee is Present");
+            workingHours=8;
         } 
         else {
             System.out.println("Employee is Absent");
         }
+        //Usecase 2: calculate_daily_employee_wage
+        calculateDailyEmployeeWage(workingHours);
+    }
+    
+    /*
+    @param : workingHours - no. of working hours
+    @desc : Calculate daily employee wage
+    */
+    private static void calculateDailyEmployeeWage(int workingHours) {
+
+        final int WAGE_PER_HOUR = 20;
+        int dailyWage = WAGE_PER_HOUR * workingHours;
+
+        System.out.println("Daily Employee Wage: " + dailyWage);
     }
 }
