c3e68c693dedd16425c6bd3dacdbf61d8ed06023
roslynpius <roslyn.pius@gmail.com>
2023-11-23 15:45:53 +0530

    Initial commit

---
+++ Employee_wage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.Employee_Wage_Computation;
+
+public class Employee_wage {   
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
