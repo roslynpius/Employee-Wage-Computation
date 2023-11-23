4ddca9bf2e39f5bac9598f42846763cbd09ec6ba
roslynpius <roslyn.pius@gmail.com>
2023-11-23 16:03:45 +0530

    Implemented USE CASE 3: Added Part Time Employee And Wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -8,20 +8,25 @@ public class Employee_wage {
 
         Random random = new Random();
         final int FULL_TIME=1;
-        int isPresent = random.nextInt(2); // 0 for absent, 1 for full time present
+        final int PART_TIME=2;
+        int isPresent = random.nextInt(3); // 0 for absent, 1 for full time present, 2 for part time present
         int workingHours=0;
 
         if (isPresent == FULL_TIME){
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present and is Full Time Employee");
             workingHours=8;
         } 
+        else if (isPresent == PART_TIME){
+            System.out.println("Employee is Present and is Part Time Employee");
+            workingHours=4;
+        }
         else {
             System.out.println("Employee is Absent");
         }
         //Usecase 2: calculate_daily_employee_wage
         calculateDailyEmployeeWage(workingHours);
     }
-    
+
     /*
     @param : workingHours - no. of working hours
     @desc : Calculate daily employee wage
