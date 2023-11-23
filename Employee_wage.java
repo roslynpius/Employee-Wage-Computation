eac008b35826bf03748806d3fe9261a791a8c55f
roslynpius <roslyn.pius@gmail.com>
2023-11-23 15:56:53 +0530

    Implemented uc1: Added Employee Present Or Absent

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,8 +1,20 @@
 package com.bridgelabz.Employee_Wage_Computation;
+import java.util.Random;
 
 public class Employee_wage {   
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+        Random random = new Random();
+        final int FULL_TIME=1;
+        int isPresent = random.nextInt(2); // 0 for absent, 1 for full time present
+
+        if (isPresent == FULL_TIME){
+            System.out.println("Employee is Present");
+        } 
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
