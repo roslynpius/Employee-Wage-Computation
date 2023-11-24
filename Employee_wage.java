3ed41d87bb39a0df8759fc41b54d29a94afbcf5d
roslynpius <roslyn.pius@gmail.com>
2023-11-24 18:14:59 +0530

    Implemented USECASE 8: Computing Employee Wage for Multiple Companies

--- Employee_wage.java
+++ Employee_wage.java
@@ -2,57 +2,60 @@ package com.bridgelabz.Employee_Wage_Computation;
 import java.util.Random;
 
 /**
- * The EmployeeWageCalculator class models the computation of employee wages based on attendance and working hours.
- * It includes methods for calculating daily wages, total monthly wage, and working hours.
+ * The EmployeeWageCalculator class models the computation of employee wages for multiple companies.
  */
 public class Employee_wage {
-    private static final int FULL_TIME = 1;
-    private static final int PART_TIME = 2;
-    private static final int REQ_WORKING_DAYS = 20;
-    private static final int REQ_WORKING_HOURS = 100;
-    private static final int WAGE_PER_HOUR = 20;
 
     /**
-     * Computes the total employee wage for a month.
+     * Computes the total employee wage for a company.
+     *
+     * @param companyName     Name of the company.
+     * @param wagePerHour     Wage per hour for the company.
+     * @param workingDays     Number of working days per month.
+     * @param workingHours    Number of working hours per day.
      */
-    public void computeEmployeeWage() {
+    public static void computeEmployeeWage(String companyName, int wagePerHour, int workingDays, int workingHours) {
         Random random = new Random();
         int totalWage = 0;
+        System.out.printf("Company: %s\n", companyName);
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
-        for (int day = 1, totalWorkingHours = 0; day <= REQ_WORKING_DAYS && totalWorkingHours < REQ_WORKING_HOURS; day++) {
+        for (int day = 1, totalWorkingHours = 0; day <= workingDays && totalWorkingHours < workingDays * workingHours; day++) {
             int isPresent = random.nextInt(3); // 0 for absent, 1 for full time present, 2 for part time present
 
-            int workingHours = switch (isPresent) {
-                case FULL_TIME -> 8;
-                case PART_TIME -> 4;
+            int workingHoursToday = switch (isPresent) {
+                case 1 -> 8;
+                case 2 -> 4;
                 default -> 0; // Employee is Absent
             };
 
-            int dailyWage = calculateDailyEmployeeWage(workingHours);
+            int dailyWage = calculateDailyEmployeeWage(wagePerHour, workingHoursToday);
             System.out.println("Day: " + day + " Wage:" + dailyWage);
             totalWage += dailyWage;
-            totalWorkingHours += workingHours;
+            totalWorkingHours += workingHoursToday;
 
-            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, dailyWage, totalWorkingHours);
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHoursToday, dailyWage, totalWorkingHours);
         }
 
-        System.out.println("Total wage for a month is " + totalWage);
+        System.out.println("Total wage for the month at " + companyName + " is " + totalWage);
+        System.out.println();
+        System.out.println();
     }
 
     /**
-     * Calculates the daily employee wage based on the given working hours.
+     * Calculates the daily employee wage based on the given wage per hour and working hours.
      *
-     * @param workingHours Number of working hours in a day.
+     * @param wagePerHour    Wage per hour.
+     * @param workingHours   Number of working hours in a day.
      * @return Daily employee wage.
      */
-    private int calculateDailyEmployeeWage(int workingHours) {
-        return WAGE_PER_HOUR * workingHours;
+    private static int calculateDailyEmployeeWage(int wagePerHour, int workingHours) {
+        return wagePerHour * workingHours;
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        Employee_wage wageCalculator = new Employee_wage();
-        wageCalculator.computeEmployeeWage();
+        // Example usage for two companies
+        computeEmployeeWage("ABC", 25, 20, 50);
+        computeEmployeeWage("XYZ", 30, 22, 25);
     }
 }
